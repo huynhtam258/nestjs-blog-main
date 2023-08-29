7b85ef470aa87eb49c00a3f7392c046d2363e81c
Huynh Manh Tam <huynhtam258@gmail.com>
2023-08-29 14:05:02 +0700

    feat: update code post service

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,11 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, Query } from '@nestjs/common';
 import { InjectRepository,  } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository, UpdateResult } from 'typeorm';
 import { User }  from './../user/entities/user.entity'
 import { Post } from './entities/post.entity';
 import { CreatePostDto } from './dto/create-post.dto';
+import { FilterPostDto } from './dto/filter-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
@@ -22,4 +24,75 @@ export class PostService {
             throw new HttpException("Can't create post", HttpStatus.BAD_REQUEST)
         }
     }
+
+    public async findAll (@Query() query: FilterPostDto): Promise<any> {
+        const items_per_page = Number(query.items_per_page) || 10;
+        const page = Number(query.page) || 1;
+        const search = query.search || '';
+        const category = Number(query.category) || null;
+
+        const skip = (page - 1) * items_per_page;
+
+        const [res, total] = await this.postRepository.findAndCount({
+            where: [
+                {
+                    title: Like('%' + search + '%')
+                },
+                {
+                    description: Like('%' + search + '%')
+                }
+            ],
+            order: { created_at: 'DESC' },
+            take: items_per_page,
+            skip: skip,
+            relations: {
+                user: true
+            },
+            select: {
+                user: {
+                    id: true,
+                    first_name: true,
+                    last_name: true,
+                    email: true,
+                    avatar: true
+                }
+            }
+        })
+
+        const lastPage = Math.ceil(total / items_per_page);
+        const nextPage = page + 1 > lastPage ? null : page + 1;
+        const prevPage = page - 1 < 1 ? null : page - 1;
+
+        return {
+            data: res,
+            total,
+            currentPage: page,
+            nextPage,
+            prevPage,
+            lastPage
+        }
+    }
+
+    public async findDetail(postId: number): Promise<Post> {
+        return await this.postRepository.findOne({
+            where: {
+                id: postId
+            },
+            relations: ['user'],
+            select: {
+                user: {
+                    id: true,
+                    first_name: true,
+                    last_name: true,
+                    email: true,
+                    avatar: true
+                }
+            }
+        })
+    }
+
+    public async update(id: number, updatePostDto: UpdatePostDto): Promise<UpdateResult> {
+        return await this.postRepository.update(id, updatePostDto)
+    }
+
 }
