b385021ea832e0cb8e7d52a78388c5bdabda9af5
HuynhManhTam <huynhtam258@gmail.com>
2025-02-16 11:59:05 +0700

    fix: config ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   apps : [{
     name   : "Backend_Gocnhinkhacbiet",
-    script : "./dist/src/main.js"
+    script : "./dist/src/main.js",
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3000,
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 5000,
+    },
   }]
 }
